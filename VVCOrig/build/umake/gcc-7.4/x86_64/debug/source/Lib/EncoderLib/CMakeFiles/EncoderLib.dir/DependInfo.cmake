1ae17e7d2cbfe37b25b9d1018f954d96bae751de
mkjubran <mjubran@birzeit.edu>
2020-01-28 13:13:56 +0000

    Ver0.0

---
+++ VVCOrig/build/umake/gcc-7.4/x86_64/debug/source/Lib/EncoderLib/CMakeFiles/EncoderLib.dir/DependInfo.cmake
@@ -0,0 +1,47 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/mjubran/HMStitching/VVCS/VVCOrig/source/Lib/EncoderLib/AQp.cpp" "/home/mjubran/HMStitching/VVCS/VVCOrig/build/umake/gcc-7.4/x86_64/debug/source/Lib/EncoderLib/CMakeFiles/EncoderLib.dir/AQp.cpp.o"
+  "/home/mjubran/HMStitching/VVCS/VVCOrig/source/Lib/EncoderLib/BinEncoder.cpp" "/home/mjubran/HMStitching/VVCS/VVCOrig/build/umake/gcc-7.4/x86_64/debug/source/Lib/EncoderLib/CMakeFiles/EncoderLib.dir/BinEncoder.cpp.o"
+  "/home/mjubran/HMStitching/VVCS/VVCOrig/source/Lib/EncoderLib/CABACWriter.cpp" "/home/mjubran/HMStitching/VVCS/VVCOrig/build/umake/gcc-7.4/x86_64/debug/source/Lib/EncoderLib/CMakeFiles/EncoderLib.dir/CABACWriter.cpp.o"
+  "/home/mjubran/HMStitching/VVCS/VVCOrig/source/Lib/EncoderLib/EncAdaptiveLoopFilter.cpp" "/home/mjubran/HMStitching/VVCS/VVCOrig/build/umake/gcc-7.4/x86_64/debug/source/Lib/EncoderLib/CMakeFiles/EncoderLib.dir/EncAdaptiveLoopFilter.cpp.o"
+  "/home/mjubran/HMStitching/VVCS/VVCOrig/source/Lib/EncoderLib/EncCu.cpp" "/home/mjubran/HMStitching/VVCS/VVCOrig/build/umake/gcc-7.4/x86_64/debug/source/Lib/EncoderLib/CMakeFiles/EncoderLib.dir/EncCu.cpp.o"
+  "/home/mjubran/HMStitching/VVCS/VVCOrig/source/Lib/EncoderLib/EncGOP.cpp" "/home/mjubran/HMStitching/VVCS/VVCOrig/build/umake/gcc-7.4/x86_64/debug/source/Lib/EncoderLib/CMakeFiles/EncoderLib.dir/EncGOP.cpp.o"
+  "/home/mjubran/HMStitching/VVCS/VVCOrig/source/Lib/EncoderLib/EncHRD.cpp" "/home/mjubran/HMStitching/VVCS/VVCOrig/build/umake/gcc-7.4/x86_64/debug/source/Lib/EncoderLib/CMakeFiles/EncoderLib.dir/EncHRD.cpp.o"
+  "/home/mjubran/HMStitching/VVCS/VVCOrig/source/Lib/EncoderLib/EncLib.cpp" "/home/mjubran/HMStitching/VVCS/VVCOrig/build/umake/gcc-7.4/x86_64/debug/source/Lib/EncoderLib/CMakeFiles/EncoderLib.dir/EncLib.cpp.o"
+  "/home/mjubran/HMStitching/VVCS/VVCOrig/source/Lib/EncoderLib/EncLibCommon.cpp" "/home/mjubran/HMStitching/VVCS/VVCOrig/build/umake/gcc-7.4/x86_64/debug/source/Lib/EncoderLib/CMakeFiles/EncoderLib.dir/EncLibCommon.cpp.o"
+  "/home/mjubran/HMStitching/VVCS/VVCOrig/source/Lib/EncoderLib/EncModeCtrl.cpp" "/home/mjubran/HMStitching/VVCS/VVCOrig/build/umake/gcc-7.4/x86_64/debug/source/Lib/EncoderLib/CMakeFiles/EncoderLib.dir/EncModeCtrl.cpp.o"
+  "/home/mjubran/HMStitching/VVCS/VVCOrig/source/Lib/EncoderLib/EncReshape.cpp" "/home/mjubran/HMStitching/VVCS/VVCOrig/build/umake/gcc-7.4/x86_64/debug/source/Lib/EncoderLib/CMakeFiles/EncoderLib.dir/EncReshape.cpp.o"
+  "/home/mjubran/HMStitching/VVCS/VVCOrig/source/Lib/EncoderLib/EncSampleAdaptiveOffset.cpp" "/home/mjubran/HMStitching/VVCS/VVCOrig/build/umake/gcc-7.4/x86_64/debug/source/Lib/EncoderLib/CMakeFiles/EncoderLib.dir/EncSampleAdaptiveOffset.cpp.o"
+  "/home/mjubran/HMStitching/VVCS/VVCOrig/source/Lib/EncoderLib/EncSlice.cpp" "/home/mjubran/HMStitching/VVCS/VVCOrig/build/umake/gcc-7.4/x86_64/debug/source/Lib/EncoderLib/CMakeFiles/EncoderLib.dir/EncSlice.cpp.o"
+  "/home/mjubran/HMStitching/VVCS/VVCOrig/source/Lib/EncoderLib/EncTemporalFilter.cpp" "/home/mjubran/HMStitching/VVCS/VVCOrig/build/umake/gcc-7.4/x86_64/debug/source/Lib/EncoderLib/CMakeFiles/EncoderLib.dir/EncTemporalFilter.cpp.o"
+  "/home/mjubran/HMStitching/VVCS/VVCOrig/source/Lib/EncoderLib/InterSearch.cpp" "/home/mjubran/HMStitching/VVCS/VVCOrig/build/umake/gcc-7.4/x86_64/debug/source/Lib/EncoderLib/CMakeFiles/EncoderLib.dir/InterSearch.cpp.o"
+  "/home/mjubran/HMStitching/VVCS/VVCOrig/source/Lib/EncoderLib/IntraSearch.cpp" "/home/mjubran/HMStitching/VVCS/VVCOrig/build/umake/gcc-7.4/x86_64/debug/source/Lib/EncoderLib/CMakeFiles/EncoderLib.dir/IntraSearch.cpp.o"
+  "/home/mjubran/HMStitching/VVCS/VVCOrig/source/Lib/EncoderLib/NALwrite.cpp" "/home/mjubran/HMStitching/VVCS/VVCOrig/build/umake/gcc-7.4/x86_64/debug/source/Lib/EncoderLib/CMakeFiles/EncoderLib.dir/NALwrite.cpp.o"
+  "/home/mjubran/HMStitching/VVCS/VVCOrig/source/Lib/EncoderLib/RateCtrl.cpp" "/home/mjubran/HMStitching/VVCS/VVCOrig/build/umake/gcc-7.4/x86_64/debug/source/Lib/EncoderLib/CMakeFiles/EncoderLib.dir/RateCtrl.cpp.o"
+  "/home/mjubran/HMStitching/VVCS/VVCOrig/source/Lib/EncoderLib/SEIEncoder.cpp" "/home/mjubran/HMStitching/VVCS/VVCOrig/build/umake/gcc-7.4/x86_64/debug/source/Lib/EncoderLib/CMakeFiles/EncoderLib.dir/SEIEncoder.cpp.o"
+  "/home/mjubran/HMStitching/VVCS/VVCOrig/source/Lib/EncoderLib/SEIwrite.cpp" "/home/mjubran/HMStitching/VVCS/VVCOrig/build/umake/gcc-7.4/x86_64/debug/source/Lib/EncoderLib/CMakeFiles/EncoderLib.dir/SEIwrite.cpp.o"
+  "/home/mjubran/HMStitching/VVCS/VVCOrig/source/Lib/EncoderLib/VLCWriter.cpp" "/home/mjubran/HMStitching/VVCS/VVCOrig/build/umake/gcc-7.4/x86_64/debug/source/Lib/EncoderLib/CMakeFiles/EncoderLib.dir/VLCWriter.cpp.o"
+  "/home/mjubran/HMStitching/VVCS/VVCOrig/source/Lib/EncoderLib/WeightPredAnalysis.cpp" "/home/mjubran/HMStitching/VVCS/VVCOrig/build/umake/gcc-7.4/x86_64/debug/source/Lib/EncoderLib/CMakeFiles/EncoderLib.dir/WeightPredAnalysis.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../../../../../source/Lib/EncoderLib/."
+  "../../../../../source/Lib/CommonLib/."
+  "../../../../../source/Lib/CommonLib/.."
+  "../../../../../source/Lib/CommonLib/./x86"
+  "../../../../../source/Lib/CommonLib/../libmd5"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/mjubran/HMStitching/VVCS/VVCOrig/build/umake/gcc-7.4/x86_64/debug/source/Lib/CommonLib/CMakeFiles/CommonLib.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
